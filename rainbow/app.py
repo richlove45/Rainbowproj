26f3a20c64bde2395e3f4cce0e0a332ec220de2a
richlove45 <richlovenkansah@gmail.com>
2021-02-06 11:53:40 -0500

    Updated rainbow html pages use captitals for the first letter of color in order to work

--- rainbow/app.py
+++ rainbow/app.py
@@ -6,33 +6,33 @@ def index():
     return "Welcome to Richlove's Rainbow Project"
 
 #def = function. Create 7 routes for the rainbow colors using this template. 
-@app.route('/red')
-def red():
-    return render_template() 
+@app.route('/Red')
+def Red():
+    return render_template("red.html") 
 
 @app.route('/Orange')
 def Orange():
-    return render_template()
+    return render_template("orange.html")
 
 @app.route('/Yellow')
 def Yellow():
-    return render_template()
+    return render_template("yellow.html")
 
 @app.route('/Green')
 def Green():
-    return render_template()
+    return render_template("green.html")
 
 @app.route('/Blue')
 def Blue():
-    return render_template()
+    return render_template("blue.html")
 
 @app.route('/Indigo')
 def Indigo():
-    return render_template()
+    return render_template("indigo.html")
 
 @app.route('/Violet')
 def Violet():
-    return render_template()
+    return render_template("violet.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
