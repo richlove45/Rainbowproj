1f292a2e6cfdbe541a2218f098c8a3d7ce462abc
richlove45 <richlovenkansah@gmail.com>
2021-03-27 13:16:31 -0400

    Solved errors. Login page loads now, working on displaying text on senseHat

--- term2finalproj/app.py
+++ term2finalproj/app.py
@@ -1,19 +1,26 @@
-from flask import Flask, render_template, current_app as app
+from flask import Flask, request, redirect, url_for, render_template, current_app as app
 from sense_hat import SenseHat
+
 app = Flask(__name__)
+
+sense = SenseHat()
+sense.show_message()
+
+@app.route('/')
+def index():
+    return "Welcome to Richlove's Login Page" 
 
 @app.route('/success/<name>')
 def success(name):
-   return 'welcome %s' % name
+   return 'Welcome %s' % name
 
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
    if request.method == 'POST':
-      user = request.form['nm']
+      user = request.form['name']
       return redirect(url_for('success',name = user))
    else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
+      return render_template("login.html")
 
 if __name__ == '__main__':
    app.run(debug = True, host='0.0.0.0')
